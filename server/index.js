c8df7dd59a89c81bf9822de8caa290bd9ebfb4ad
Guy Hadas <guyhadas195@gmail.com>
2023-02-26 15:55:43 -0500

    Add .nvmrc file

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const path = require('path')
 const https = require('https')
 
 const express = require('express')
-const axios = require('axios');
+const axios = require('axios')
 
 const app = express()
 
